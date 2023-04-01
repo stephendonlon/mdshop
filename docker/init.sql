77c2ad682d91190f27d9d1cb3bcbb8d0f302c186
Stephen Donlon <donlon1984@gmail.com>
2023-04-01 09:26:38 +0100

    Created initial setup for mysql docker

--- docker/init.sql
+++ docker/init.sql
@@ -1,14 +1,13 @@
+-- Create user
+CREATE USER 'myuser'@'%' IDENTIFIED BY 'mypassword';
+
 -- Create cart schema
 CREATE DATABASE IF NOT EXISTS cart;
-GRANT ALL PRIVILEGES ON cart.* TO 'myuser'@'%' IDENTIFIED BY 'myuserpassword';
-GRANT ALL PRIVILEGES ON cart.* TO 'myuser'@'localhost' IDENTIFIED BY 'mypasswd';
-
+GRANT ALL PRIVILEGES ON cart.* TO 'myuser'@'%' WITH GRANT OPTION;
 
 -- Create cart_command schema
 CREATE DATABASE IF NOT EXISTS cart_command;
-GRANT ALL PRIVILEGES ON cart_command.* TO 'myuser'@'%' IDENTIFIED BY 'myuserpassword';
-GRANT ALL PRIVILEGES ON cart_command.* TO 'myuser'@'localhost' IDENTIFIED BY 'mypasswd';
-
+GRANT ALL PRIVILEGES ON cart_command.* TO 'myuser'@'%' WITH GRANT OPTION;
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
